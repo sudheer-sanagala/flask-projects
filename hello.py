c937d5864c19729531b51ca951eac0928c034f73
Sudheer Sanagala <ssanagala@specialized.com>
2020-12-26 18:38:32 -0800

    html change

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 
 app = Flask(__name__)
 
+movies = {1: 'Toy story', 2: 'The Raid', 3: 'Hero',
+            4: 'Ip Man', 5: 'Kung Fu Panda'}
+
+fruits = ['apples','banana','grapes','oranges']
+
 @app.route("/check")
 def check():
     return f"OK, the server is up and running."
@@ -17,6 +22,28 @@ def return_dict():
     """
     return {"Key":"Value"}
     pass
+
+@app.route("/fruits")
+def fruit_list():
+    """
+    docstring
+    """
+    return render_template('test.html',len = len(fruits),fruits=fruits)
+    pass
+
+@app.route("/fruits/<string:fruit>")
+def get_fruit(fruit):
+    return f'You selected {fruit}'
+
+
+@app.route("/movies")
+def get_movies():
+    """
+    docstring
+    """
+    return render_template('movies.html',movies=movies)
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
